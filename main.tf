7b3c994126dfe5dccf051c2c59007933d926e605
arun <mrarunmalhotra@gmail.com>
2024-05-01 16:09:22 +0530

    added new file

--- main.tf
+++ main.tf
@@ -1,30 +1,8 @@
-#terraform {
-
-#  required_providers {
-
- #   aws = {
-
-  #    source  = "hashicorp/aws"
-
-   #   version = "~> 4.16"
-
-   # }
-
- # }
-
-
-
-  #required_version = ">= 1.2.0"
-
-#}
-
-
-
 provider "aws" {
 
   region  = "us-west-2"
 
-  profile = "jack.roper"
+  #profile = "jack.roper"
 
 }
 
