85e0f2c3b6ebb47425345516e9c19ce4973433e2
arun <mrarunmalhotra@gmail.com>
2024-05-01 16:17:51 +0530

    added new file

--- main.tf
+++ main.tf
@@ -3,8 +3,8 @@ provider "aws" {
   region  = "us-west-2"
 
   #profile = "jack.roper"
-  access_key = "AKIASKGRHVTQBUUTATM3"
-  secret_key = "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"
+  #access_key = "AKIASKGRHVTQBUUTATM3"
+  #secret_key = "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"
 
 }
 
